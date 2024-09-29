4d1be50fc32a3b05d32c2bd5db4f226c39471f64
Connor Boyle <connor.bo@gmail.com>
2024-09-29 00:23:05 -0700

    Change title to STAR voting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {getPreferences, getScores, getSTARWinner, getTopScorers, sortScores, TopScoresResult} from "./star.ts";
 
@@ -116,15 +114,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo"/>
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>STAR Voting</h1>
       <div className="card">
         <button onClick={() => getSTARWinner(votes.map(vote => vote.scores))}>
           Calculate winner
